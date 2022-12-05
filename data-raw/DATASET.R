1d2bea9e136cd031c38e3e6967adccd0f0b6a894
Niccolò Salvini <niccolo.salvini27@gmail.com>
2022-12-05 10:44:56 +0100

    update `mock_data_core`
    fix "provincia" levels

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,16 +7,23 @@ library(dplyr)
 library(forcats)
 library(tidyr)
 library(stringr)
+library(forcats)
 
 ## ask me for this!
 ## I have cut if for you!
-mock_data_core  = read_csv("/Users/niccolo/Desktop/r_projects/corebd/sample_bdncp_100000.csv") %>%
+mock_data_core <- read_csv("/Users/niccolo/Desktop/r_projects/corebd/sample_bdncp_100000.csv") %>%
   saveRDS(file = here("data-raw", "mock_data_core.rds"))
 
 
-mock_data_core = read_rds(here("data-raw", "mock_data_core.rds")) %>%
+mock_data_core <- read_rds(here("data-raw", "mock_data_core.rds")) %>%
   select(-...1, -ends_with(".y")) %>%
-  rename_if(endsWith(names(.), ".x"), ~str_remove(., ".x"))
+  rename_if(endsWith(names(.), ".x"), ~ str_remove(., ".x")) %>%
+  mutate(
+    provincia = fct_recode(provincia,
+      "REGGIO NELL'EMILIA" = "REGGIO NELLEMILIA",
+      "L'AQUILA" = "LAQUILA"
+    )
+  )
 
 
 ## preps
