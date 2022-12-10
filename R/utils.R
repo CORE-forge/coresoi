f69a09ba379941db19d290b94036362bc3903d31
Niccolò Salvini <niccolo.salvini27@gmail.com>
2022-12-11 00:12:25 +0100

    add function corpus

--- R/utils.R
+++ R/utils.R
@@ -17,27 +17,61 @@ get_emergency_id_from_date <- function(outbreak_starting_date) {
 }
 
 
-# TODO: check this resource to dynamically get emergency scenarios https://emergenze.protezionecivile.gov.it/it/
-# TODO: Italy or in general? those are italian contract so we should take care of only Italy!
-
-#' Desume Emergency name from its date
-#' @keywords internal
+#' @title get the emergency date from input string
+#' @description gets the outbreak starting date from emergency name input string
+#' @param emergency_name  emergency name character string
+#' @return a lubridate class dmy date.
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  emergency_dates("Coronavirus")
+#'  emergency_dates("Codronavilus")
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[lubridate]{ymd}}
+#' @rdname emergency_dates
 #' @export
-get_emergency_name_from_date <- function(outbreak_starting_date) {
-  if (lubridate::as_date(outbreak_starting_date) %within% lubridate::interval(start = lubridate::ymd("2020-01-31"), end = lubridate::ymd("2022-03-31"))) {
-    return("Coronavirus")
-  } else if (lubridate::as_date(outbreak_starting_date) %within% lubridate::interval(start = lubridate::ymd("2022-02-24"), end = lubridate::today())) {
-    return("Ucraine - Russia war")
-  } else if (lubridate::as_date(outbreak_starting_date) %within% lubridate::interval(start = lubridate::ymd("2009-04-06"), end = lubridate::ymd("2012-08-31"))) {
-    return("L'Aquila Earthquake")
+#' @importFrom lubridate ymd dmy
+emergency_dates <- function(emergency_name) {
+  # create a named list of emergency names and their corresponding dates
+  # TODO add emergenze umanitarie
+  emergency_list <- list(
+    "Coronavirus" = lubridate::ymd("2020-01-31"),
+    "Terremoto Aquila" = lubridate::ymd("2017-06-30"),
+    "Terremoto Ischia" = lubridate::dmy("21/08/2017"),
+    "Terremoto Centro Italia 2016-2017" = lubridate::dmy("24/08/2016"),
+    "Terremoto Emilia-Romagna e Lombardia 2012" = lubridate::dmy("20/05/2012"),
+    "Etna - Eruzione 2008-2009" = lubridate::dmy("13/05/2008"),
+    "Etna - Eruzione 2006-2007" = lubridate::dmy("14/07/2006"),
+    "Etna - Eruzione 2002-2003" = lubridate::dmy("28/10/2002"),
+    "Stromboli - Eruzione 2007" = lubridate::dmy("24/08/2016"),
+    "Terremoto Emilia-Romagna e Lombardia 2012" = lubridate::dmy("20/05/2012"),
+    "Neve al centro-sud 2012"  = lubridate::dmy("31/01/2012"),
+    "Alluvione di Messina" = lubridate::dmy("01/10/2009"),
+    "Rimozione delle ecoballe nel Golfo di Follonica 2020" = lubridate::dmy("22/07/2020"),
+    "Incendi stagione estiva 2017" = lubridate::dmy("01/07/2017"),
+    "Naufragio della Concordia" = lubridate::dmy("13/01/2012"),
+    "Incendi stagione estiva 2017" = lubridate::dmy("01/07/2017")
+  )
+
+  # use agrep to find approximate matches for the input emergency name
+  emergency_match <- agrep(emergency_name, names(emergency_list), max.distance = 0.3)
+
+  # check if any approximate matches were found
+  if (length(emergency_match) > 0) {
+    # if matches were found, return the date corresponding to the first match
+    return(emergency_list[emergency_match[1]])
   } else {
-    return("Other")
+    # if no matches were found, return an error message
+    return("Error: Emergency not found in list.")
   }
 }
 
-# TODO function to grab latest data update
-# TODO: need to think a way to open this thing to other countries besides Italy
 
+
+
+# TODO function to grab latest data update
 
 from_aggregation_type_to_istat_code <- function(variables) {
 
@@ -49,7 +83,7 @@ from_aggregation_type_to_eu_nc <- function(variables) {
 }
 
 
-#' @title Generate commmon schema for indicator
+#' generate indicator schema
 #' @keywords internal
 #' @export
 generate_indicator_schema <- function(.data, indicator_id, indicator_name, aggregation_type, outbreak_starting_date, ...) {
