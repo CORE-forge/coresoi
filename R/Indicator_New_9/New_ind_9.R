1d1ba9091a1f8984376dc7a9ec1fb4e218fd9e5f
giuliogcantone <prgcan@gmail.com>
2023-02-09 15:24:24 +0100

    fixed

--- R/Indicator_New_9/New_ind_9.R
+++ R/Indicator_New_9/New_ind_9.R
@@ -1,10 +1,13 @@
+compute_ttest <- function(data, mu, alternative) {
+  stats::t.test(x=data, mu=mu, alternative="greater")$p.value
+}
+
 #' @title Lenghty Contracts
 #' @description Something
 #' @param data data to be passed, expects tibble
 #' @param publication_date The date when the tender was published
 #' @param stat_unit the statistical unit of measurement (can be a vector of grouping variables), i.e. variable to group by
 #' @param cpv something
-#' @param cpv_focus something
 #' @param eff_start Effective start of the execution of the contract
 #' @param eff_end Effective end of the execution of the contract
 #' @param emergency_name Emergency
@@ -13,29 +16,31 @@
 #' @examples
 #' \dontrun{
 #' if (interactive()) {
-#'   ind_4(
+#'   ind_9(
 #'     data = mock_data_core, publication_date = data_pubblicazione,
 #'     stat_unit = cf_amministrazione_appaltante,
-#'     cpv = ..., cpv_focus = "33",
-#'     eff_start = ..., eff_end = ...,
+#'     cpv = cod_cpv, eff_start = ..., eff_end = ...,
 #'     emergency_name = ...
 #'   )
 #' }
 #' }
 #' @seealso
+#'  \code{\link[dplyr]{mutate}}, \code{\link[dplyr]{if_else}}, \code{\link[dplyr]{filter}}, \code{\link[dplyr]{pull}}, \code{\link[dplyr]{group_by}}, \code{\link[dplyr]{summarise}}, \code{\link[dplyr]{context}}, \code{\link[dplyr]{rename}}
 #'  \code{\link[lubridate]{ymd}}
-#'  \code{\link[dplyr]{filter}}, \code{\link[dplyr]{mutate}}, \code{\link[dplyr]{if_else}}, \code{\link[dplyr]{group_by}}, \code{\link[dplyr]{summarise}}, \code{\link[dplyr]{context}}
 #'  \code{\link[forcats]{as_factor}}
-#' @rdname ind_4
+#'  \code{\link[stringr]{str_sub}}
+#'  \code{\link[rlang]{as_string}}
+#' @rdname ind_9
 #' @export
+#' @importFrom dplyr mutate if_else filter pull group_by summarise n rename
 #' @importFrom lubridate ymd
-#' @importFrom dplyr filter mutate if_else group_by summarise n
 #' @importFrom forcats as_factor
+#' @importFrom stringr str_sub
+#' @importFrom rlang as_string
 ind_9 <- function(data,
                   publication_date,
                   stat_unit,
                   cpv,
-                  cpv_focus = "33",
                   eff_start,
                   eff_end,
                   emergency_name) {
@@ -51,7 +56,7 @@ ind_9 <- function(data,
         true = "post",false = "pre"),
       prepost = forcats::as_factor(prepost),
       flagdivision = dplyr::if_else(
-        stringr::str_sub({{ cpv }}, start = 1, end = 2) == cpv_focus,
+        stringr::str_sub({{ cpv }}, start = 1, end = 2) == emergency_scenario$cpv,
         true = 1, false = 0),
       contract_duration = dplyr::if_else(
         {{ eff_end }} < {{ eff_start }},
@@ -61,8 +66,8 @@ ind_9 <- function(data,
       ) -> data_out
 
   grand_mean <- data_out %>%
-    dplyer::filter(prepost == "post") %>%
-    dplyer::pull(contract_duration) %>%
+    dplyr::filter(prepost == "post") %>%
+    dplyr::pull(contract_duration) %>%
     mean(na.rm = TRUE)
 
   data_out %>%
@@ -77,39 +82,22 @@ ind_9 <- function(data,
       # test not to be performed if nnomiss == 1: not working, why?
       ttest = dplyr::if_else(nnomiss == 1,
                              true = NA_real_,
-                             false = compute_ttest(data=contract_duration, mu=grand_mean))
-      # the ideal would be using 'comp_grandmean' (see below), but it's computer intensive
-    ) -> out
-  # return()
-  return(list(data=data_out, out=out))
-
-  ### C'è un problema con questo indicatore: io non so cosa andare a mettere come output dello schema
-
-  compute_ttest <- function(data, mu, alternative) {
-    stats::t.test(x=data, mu=mu, alternative="greater")$p.value
-  }
-
-  comp_grandmean <- function(y, x) {
-    # y: continuous variable (contract duration)
-    # x: factor (companies/contracting authorities)
-    a <- stats::aov(y~x)
-    out <- multcomp::glht(a, linfct = multcomp::mcp(x = "GrandMean"), alternative="greater")
-    summ_out <- multcomp::summary(out) #computer intensive
-    return(summ_out)
-  }
-
- %>%
+                             false = compute_ttest(
+                               data=contract_duration, mu=grand_mean))
+    ) %>%
     generate_indicator_schema(
       indicator_id = indicator_id,
       indicator_name = indicator_name,
       {{ stat_unit }},
-      ind_9_mean,
+      output = ttest,
       aggregation_type = rlang::as_string(aggregation_type),
-      outbreak_starting_date = outbreak_starting_date
+      emergency = emergency_name
     ) %>%
     dplyr::rename(
-      indicator_value = ind_9_mean,
+      indicator_value = output,
       aggregation_name = {{ stat_unit }}
     ) %>%
     return()
 }
+
+sinew::makeOxygen(ind_9)
