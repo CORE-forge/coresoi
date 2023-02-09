f635f02e7f7cd9da7570bbd9f9b7fccac3064cfa
giuliogcantone <prgcan@gmail.com>
2023-02-09 15:01:20 +0100

    Fixed Ind 8

---
+++ R/Indicator New_8/NEW_ind_8_FIXED.R
@@ -0,0 +1,111 @@
+#' @title Contracts that got a variation
+#' @description The contract changed
+#' @param data a dataset
+#' @param publication_date The date when the tender was published
+#' @param stat_unit Identifier of agency or winners
+#' @param variant_date Date of the variation of the contract
+#' @param winning_date Date when the contract has a winner
+#' @param months_win ???
+#' @param emergency_name the date of the emergency outbreak, Default: lubridate::ymd("2017-06-30")
+#' @return indicator schema as from `generate_indicator_schema`
+#' @examples
+#' \dontrun{
+#' if (interactive()) {
+#'   data("mock_data_core")
+#'   ind_3(
+#'     data = mock_data_core,
+#'     publication_date = data_pubblicazione,
+#'     stat_unit = cf_amministrazione_appaltante,
+#'     variant_date = data_approvazione_variante,
+#'     winning_date = data_aggiudicazione_definitiva,
+#'     months_win = 6,
+#'     emergency_name = ???
+#'   )
+#' }
+#' }
+#' @seealso
+#'  \code{\link[lubridate]{ymd}}, \code{\link[lubridate]{character(0)}}, \code{\link[lubridate]{interval}}, \code{\link[lubridate]{period}}
+#'  \code{\link[tidyr]{nest}}
+#'  \code{\link[dplyr]{mutate}}, \code{\link[dplyr]{across}}, \code{\link[dplyr]{reexports}}, \code{\link[dplyr]{if_else}}, \code{\link[dplyr]{group_by}}
+#'  \code{\link[forcats]{as_factor}}
+#' @rdname ind_3
+#' @export
+#' @importFrom lubridate ymd interval years %within% %m+%
+#' @importFrom tidyr unnest
+#' @importFrom dplyr mutate across starts_with if_else group_by
+#' @importFrom forcats as_factor
+
+
+ind_8 <- function(data,
+                  publication_date,
+                  stat_unit,
+                  variant_date,
+                  winning_date,
+                  emergency_name,
+                  months_win = 6){
+
+
+indicator_id <- 8
+indicator_name <- "Pre-existing contracts modified after the crisis"
+aggregation_type <- quo_squash(enquo({{ stat_unit }}))
+emergency_scenario <- emergency_dates({{ emergency_name }})
+
+# select useful variables and remove duplicates
+# @giulio: how to generalise this?
+# data <- data %>%
+#  filter(!is.na({{ winning_date }})) %>%
+#  select(cig, cf_amministrazione_appaltante, data_pubblicazione,
+#         data_aggiudicazione_definitiva, id_variante, data_approvazione_variante,
+#         codice_fiscale) %>%
+#  distinct()
+# Let's discuss this later!
+
+#if (aggregation_type == "cf_amministrazione_appaltante") {
+#  data <- data %>%
+#    dplyr::select(-codice_fiscale) %>%
+#    dplyr::distinct()
+#}
+
+#Let's discuss this later
+
+{{ data }} %>%
+  dplyr::mutate(
+    prepost = dplyr::if_else(
+      lubridate::ymd({{ publication_date }}) >= emergency_scenario$em_date,
+      true = "post",
+      false = "pre"),
+    prepost = forcats::as_factor(prepost),
+    flag_modif = dplyr::if_else(
+      prepost == "pre" & lubridate::ymd({{ variant_date }}) %within%
+        lubridate::interval(emergency_scenario$em_date,
+                            emergency_scenario$em_date %m+%
+                              months(months_win)),
+      true = 1,
+      false = 0),
+  ) %>%
+  dplyr::group_by({{ stat_unit }}) %>%
+  dplyr::summarise(
+    n = dplyr::n(),
+    npre = sum(prepost == "pre"),
+    ncig = dplyr::n_distinct(cig),
+#    ncig_pre = data.table::uniqueN(cig[prepost == "pre"]),
+    nmod = sum(flag_modif == 1),
+#    ncig_mod = data.table::uniqueN(cig[flag_modif == 1]),
+    # prop_mod = nmod/npre,
+    rf_value = 1*(nmod>0)
+  ) %>%
+  generate_indicator_schema(
+    indicator_id = indicator_id,
+    indicator_name = indicator_name,
+    output = ref_value,
+    {{ stat_unit }},
+    aggregation_type = as_string(aggregation_type),
+    outbreak_starting_date = outbreak_starting_date
+  ) %>%
+  dplyr::rename(
+    indicator_value = output,
+    aggregation_name = {{ stat_unit }}
+  ) %>%
+  return()
+# return(list(data=data_out, out=out))
+}
