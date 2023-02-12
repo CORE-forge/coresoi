0867cd4063a7192fcba5ae23088ef8ce4d65e1cf
Niccolò Salvini <niccolo.salvini27@gmail.com>
2023-02-12 19:04:08 +0100

    initial commit fro `ind_9`

--- R/09-lengthy-contract.R
+++ R/09-lengthy-contract.R
@@ -1,3 +1,228 @@
-ind_9 <- function(variables) {
-
+#' compute t test
+#' @description  compute  test pvalue and estimate in piped expression
+#' @keywords internal
+#' @export
+compute_ttest <- function(mean_to_compare, ground_mean) {
+  data %>%
+    stats::t.test(x=mean_to_compare, mu=ground_mean, alternative="greater")$p.value
 }
+
+#' @title compute Lenghty Contracts indicator
+#' @description The indicator reveals whether the duration of the contract is significantly longer than the average length of awarded contracts after the emergency.
+#' @param data bndcp data
+#' @param publication_date The date when the tender was published
+#' @param stat_unit the statistical unit of measurement (can be a vector of grouping variables), i.e. variable to group by
+#' @param cpv Common Procurement Vocabulary. The main vocabulary is based on a tree structure made up with codes of up to 9 digits (an 8 digit code plus a check digit). This combination of digits is associated with a wording that describes the type of supplies, works or services defining the subject of the contract
+#' @param eff_start Effective start of the execution of the contract
+#' @param eff_end Effective end of the execution of the contract
+#' @param emergency_name emergency name character string for which you want to evaluate the indicator, e.g. "Coronavirus" "Terremoto Aquila"
+#' @return indicator schema as from `generate_indicator_schema`
+#' @examples
+#' \dontrun{
+#' if (interactive()) {
+#'   ind_9(
+#'     data = mock_data_core, publication_date = data_pubblicazione,
+#'     stat_unit = cf_amministrazione_appaltante,
+#'     cpv = cod_cpv,
+#'     eff_start = ...,
+#'     eff_end = ...,
+#'     emergency_name = ...
+#'   )
+#' }
+#' }
+#' @seealso
+#'  \code{\link[dplyr]{mutate}}, \code{\link[dplyr]{if_else}}, \code{\link[dplyr]{filter}}, \code{\link[dplyr]{pull}}, \code{\link[dplyr]{group_by}}, \code{\link[dplyr]{summarise}}, \code{\link[dplyr]{context}}, \code{\link[dplyr]{rename}}
+#'  \code{\link[lubridate]{ymd}}
+#'  \code{\link[forcats]{as_factor}}
+#'  \code{\link[stringr]{str_sub}}
+#'  \code{\link[rlang]{as_string}}
+#' @rdname ind_9
+#' @export
+#' @importFrom dplyr mutate if_else filter pull group_by summarise n rename
+#' @importFrom lubridate ymd
+#' @importFrom forcats as_factor
+#' @importFrom stringr str_sub
+#' @importFrom rlang as_string
+ind_9 <- function(data,
+                  publication_date,
+                  stat_unit,
+                  cpv,
+                  eff_start,
+                  eff_end,
+                  emergency_name) {
+  indicator_id <- 9
+  indicator_name <- "Lengthy contracts"
+  aggregation_type <- quo_squash(enquo(stat_unit))
+  emergency_scenario <- emergency_dates(emergency_name)
+
+  data %>%
+    dplyr::mutate(
+      prepost = dplyr::if_else(
+        lubridate::ymd({{ publication_date }}) >= emergency_scenario$em_date,
+        true = "post",false = "pre"),
+      prepost = forcats::as_factor(prepost),
+      flagdivision = dplyr::if_else(
+        stringr::str_sub({{ cpv }}, start = 1, end = 2) == "33",
+        true = 1, false = 0),
+      contract_duration = dplyr::if_else(
+        {{ eff_end }} < {{ eff_start }},
+        true = NA_real_, false =
+          1 + as.numeric({{ eff_end }} - {{ eff_start }})
+      )
+    ) -> data_out
+
+  grand_mean <- data_out %>%
+    dplyr::filter(prepost == "post") %>%
+    dplyr::pull(contract_duration) %>%
+    mean(na.rm = TRUE)
+
+  data_out %>%
+    dplyr::filter(prepost == "post" & !is.na(contract_duration)) %>%
+    # dplyr::filter(flagdivision == 1) %>%  #commented for the moment (very few data)
+    dplyr::group_by({{ stat_unit }}) %>%
+    dplyr::summarise(
+      ntot = dplyr::n(),
+      nnomiss = sum(!is.na(contract_duration)),
+      mean = mean(contract_duration, na.rm = TRUE),
+      ttest  = t.test(x = contract_duration[nnomiss!=1], mu =grand_mean)$p.value)
+
+      ttest = dplyr::if_else(nnomiss == 1,
+                             true = NA_real_,
+                             false = do(tidy(t.test(value ~ group, data = .)))$p.value))
+
+
+      ttest = dplyr::if_else(nnomiss == 1,
+                             true = NA_real_,
+                             false = t.test(y=contract_duration, mu=grand_mean, alternative="greater")$p.value)
+    ) %>%
+    generate_indicator_schema(
+      indicator_id = indicator_id,
+      indicator_name = indicator_name,
+      ttest,
+      {{ stat_unit }},
+      aggregation_type = rlang::as_string(aggregation_type),
+      emergency = emergency_name
+    ) %>%
+    dplyr::rename(
+      indicator_value = ttest,
+      aggregation_name = {{ stat_unit }}
+    ) %>%
+    return()
+}
+
+
+ind_9(
+ data = mock_data_core,
+ publication_date = data_pubblicazione,
+ stat_unit = cf_amministrazione_appaltante,
+ cpv = cod_cpv,
+ eff_start =data_inizio_effettiva,
+ eff_end = data_effettiva_ultimazione,
+ emergency_name = "coronavirus"
+)
+
+
+
+
+
+
+
+
+
+indicator_id <- 9
+indicator_name <- "Lengthy contracts"
+emergency_scenario <- emergency_dates("coronavirus")
+
+mock_data_core %>%
+  dplyr::mutate(
+    prepost = dplyr::if_else(
+      lubridate::ymd(data_pubblicazione) >= emergency_scenario$em_date,
+      true = "post",false = "pre"),
+    prepost = forcats::as_factor(prepost),
+    flagdivision = dplyr::if_else(
+      stringr::str_sub(cod_cpv, start = 1, end = 2) == "33",
+      true = 1, false = 0),
+    contract_duration = dplyr::if_else(
+      data_effettiva_ultimazione < data_inizio_effettiva,
+      true = NA_real_, false =
+        1 + as.numeric(data_effettiva_ultimazione - data_inizio_effettiva)
+    )
+  ) -> data_out
+
+grand_mean <- data_out %>%
+  dplyr::filter(prepost == "post") %>%
+  dplyr::pull(contract_duration) %>%
+  mean(na.rm = TRUE)
+
+wowo =data_out %>%
+  dplyr::filter(prepost == "post" & !is.na(contract_duration)) %>%
+  # dplyr::filter(flagdivision == 1) %>%  #commented for the moment (very few data)
+  dplyr::group_by(cf_amministrazione_appaltante) %>%
+  dplyr::mutate(
+    ntot = dplyr::n(),
+    mean = mean(contract_duration, na.rm = TRUE),
+    values = list(contract_duration),
+    nnomiss = sum(!is.na(contract_duration)),
+    ttest  = t.test(x = contract_duration[nnomiss!=1], mu =grand_mean)$p.value)
+
+
+
+wowo =data_out %>%
+  dplyr::filter(prepost == "post" & !is.na(contract_duration)) %>%
+  # dplyr::filter(flagdivision == 1) %>%  #commented for the moment (very few data)
+  dplyr::group_by(cf_amministrazione_appaltante) %>%
+  dplyr::summarise(
+    values = list(contract_duration)) %>%
+    ttest  = map_dbl(., ~ t.test(.x$E[[1L]], .x$I[[1L]])$p.value)
+
+
+
+
+
+
+    ttest = dplyr::if_else(nnomiss == 1,
+                           true = NA_integer_,
+                           false =  t.test(x=contract_duration, mu=grand_mean, alternative="greater", data = .)$p.value)
+  )
+    ttest = case_when(
+      nnomiss == 1  ~ NA_integer_,
+      nnomiss == 2 ~ stats::t.test(x=.$contract_duration, mu=171.7222, alternative="greater", na.rm  = T)$p.value,
+      .default = "other"
+    )
+    ttest = case_when(
+      nnomiss == 1  ~ 23,
+      nnomiss == 2 ~ stats::t.test(x=.$contract_duration, mu=171.7222, alternative="greater", na.rm  = T)$p.value,
+      .default = "other"
+    ))
+    ttest = dplyr::if_else(nnomiss == 1,
+                           true = NA_character_,
+                           false = list(t.test(x=contract_duration, mu=171.7222, alternative="greater", na.rm  = T)))
+    )
+
+
+
+
+
+    test = purrr::if(is.na(contract_duration) ~ t.test(x=contract_duration, mu=grand_mean, alternative="greater", data = .)$p.value, ~NA))
+
+
+
+
+    #to be compared with grand_mean
+    # median = median(contract_duration, na.rm = TRUE),
+    # test not to be performed if nnomiss == 1: not working, why?
+    ttest = dplyr::if_else(condition = nnomiss == 1, true = NA_character_, false = t.test(x=contract_duration, mu=grand_mean, alternative="greater", data = .)$p.value)
+    )
+  generate_indicator_schema(
+    indicator_id = indicator_id,
+    indicator_name = indicator_name,
+    ttest,
+    {{ stat_unit }},
+    aggregation_type = rlang::as_string(aggregation_type),
+    emergency = emergency_name
+  ) %>%
+  dplyr::rename(
+    indicator_value = ttest,
+    aggregation_name = {{ stat_unit }}
+  ) %>%
+  return()
--- tests/testthat/test-09-lengthy-contract.R
+++ tests/testthat/test-09-lengthy-contract.R
@@ -1,3 +1,246 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(dplyr)
+
+expect_row_number <- function(object, n) {
+  act <- quasi_label(rlang::enquo(object), arg = "object")
+
+  act$row_number <- dim(act$val)[1]
+  if (act$row_number == n) {
+    succeed()
+    return(invisible(act$val))
+  }
+
+  message <- sprintf("%s has row_number %i, not row_number %i.", act$lab, act$row_number, n)
+  fail(message)
+}
+
+
+expect_col_number <- function(object, n) {
+  act <- quasi_label(rlang::enquo(object), arg = "object")
+
+  act$col_number <- dim(act$val)[2]
+  if (act$col_number == n) {
+    succeed()
+    return(invisible(act$val))
+  }
+
+  message <- sprintf("%s has col_number %i, not col_number %i.", act$lab, act$col_number, n)
+  fail(message)
+}
+
+
+expect_within_range <- function(object, min, max) {
+  act <- quasi_label(rlang::enquo(object), arg = "object")
+
+  act$indicator_min <- min(act$val$indicator_value)
+  act$indicator_max <- max(act$val$indicator_value)
+
+  if (any(between(act$val$indicator_value, min, max))) {
+    succeed()
+    return(invisible(act$val))
+  }
+
+  message <- sprintf("`indicator_value` in %s does not stay in [%i - %i] indeed min-max range is [%i -  %i]", act$lab, act$indicator_min, act$indicator_max, min, max)
+  fail(message)
+}
+
+test_that("check `ind_9()` are 12 columns as according to `generate_indicator_schema()`s with `stat_unit` being **provincia**", {
+  expect_col_number(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = provincia,
+        emergency_name = "coronavirus",
+        test_type = "fisher"
+      )
+    }), 12
+  )
+})
+
+
+
+test_that("check `ind_9()` are 12 columns as according to `generate_indicator_schema()`s  with `stat_unit` being **cf_amministrazione_appaltante**", {
+  expect_col_number(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = cf_amministrazione_appaltante,
+        emergency_name = "coronavirus",
+        test_type = "fisher"
+      )
+    }), 12
+  )
+})
+
+
+
+test_that("check column names are as according to pre determined schema", {
+  col_names <- c(
+    "indicator_id", "indicator_name", "indicator_value", "aggregation_name",
+    "aggregation_id", "aggregation_type", "emergency_id", "emergency_name",
+    "country_id", "country_name", "indicator_last_update",
+    "data_last_update"
+  )
+
+  expect_equal(
+    suppressWarnings({
+      names(ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = cf_amministrazione_appaltante,
+        emergency_name = "coronavirus",
+        test_type = "fisher"
+      ))
+    }), col_names, tolerance=0.8
+  )
+})
+
+
+
+test_that("check if `indicator_value` lays inbetween min/max values accroding to test chosen, Barnard, (slow, only on a 10000 top obs)", {
+  expect_within_range(
+    suppressWarnings({
+      ind_9(
+        # only 10000 obs since this is time consuming
+        data = mock_data_core %>% head(10000),
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = provincia,
+        emergency_name = "coronavirus",
+        test_type = "barnard"
+      )
+    }),
+    min = 0, max = 1
+  )
+})
+
+
+
+test_that("check if `indicator_value` lays inbetween min/max values accroding to test chosen (Fisher)", {
+  expect_within_range(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = cf_amministrazione_appaltante,
+        emergency_name = "coronavirus",
+        test_type = "fisher"
+      )
+    }),
+    min = 0, max = 1
+  )
+})
+
+
+
+test_that("check if `indicator_value` lays inbetween min/max values accroding to test chosen (Z-test proportional test)", {
+  expect_within_range(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = cf_amministrazione_appaltante,
+        emergency_name = "coronavirus",
+        test_type = "fisher"
+      )
+    }),
+    min = 0, max = 1
+  )
+})
+
+
+
+
+test_that("check if the number of rows is coherent with the aggregation level `provincia`", {
+  expect_row_number(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = provincia,
+        emergency_name = "coronavirus",
+        test_type = "fisher"
+      )
+    }),
+    n = 108
+  )
+})
+
+
+test_that("check if the number of rows is coherent with the aggregation level (`cf_amministrazione_appaltante`)", {
+  expect_row_number(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = cf_amministrazione_appaltante,
+        emergency_name = "coronavirus",
+        test_type = "fisher"
+      )
+    }),
+    n = 3227
+  )
+})
+
+## test for different scenarios
+
+test_that("check if `indicator_value` lays inbetween min/max values accroding to test chosen", {
+  expect_within_range(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = cf_amministrazione_appaltante,
+        emergency_name = "terremoto aquila",
+        test_type = "fisher"
+      )
+    }),
+    min = 0, max = 1
+  )
+})
+
+
+
+test_that("check if `indicator_value` lays inbetween min/max values accroding to test chosen AND it is consistent with a different scenario", {
+  expect_within_range(
+    suppressWarnings({
+      ind_9(
+        data = mock_data_core,
+        cpv = cod_cpv,
+        publication_date = data_pubblicazione,
+        stat_unit = cf_amministrazione_appaltante,
+        emergency_name = "terremoto aquila",
+        test_type = "z-test"
+      )
+    }),
+    min = 0, max = 1
+  )
+})
+
+
+
+test_that("check if the indicator table, in its column `emergency_name` and `emergency_id` is coherent with the change in emergency scenario", {
+  expect_equal(
+    ind_9(
+      data = mock_data_core,
+      cpv = cod_cpv,
+      publication_date = data_pubblicazione,
+      stat_unit = provincia,
+      emergency_name = "terremoto ischia",
+      test_type = "fisher"
+    ) %>% distinct(emergency_name, emergency_id) %>% flatten(),
+    list(
+      emergency_name = "Terremoto Ischia",
+      emergency_id = 3
+    )
+  )
 })
