2a273225be996deeb510296d3dc36d5b3578a019
Simone Del Sarto <simone.delsarto@unipg.it>
2023-02-16 10:50:21 +0100

    Commented test code

--- R/09-lengthy-contract.R
+++ R/09-lengthy-contract.R
@@ -102,54 +102,54 @@ ind_9 <- function(data,
 
 
 ## source("R/utils.R")
-indicator_id <- 9
-indicator_name <- "Lengthy contracts"
-emergency_scenario <- emergency_dates("coronavirus")
-
-mock_data_core %>%
-  dplyr::mutate(
-    prepost = dplyr::if_else(
-      lubridate::ymd(data_pubblicazione) >= emergency_scenario$em_date,
-      true = "post",false = "pre"),
-    prepost = forcats::as_factor(prepost),
-    flagdivision = dplyr::if_else(
-      stringr::str_sub(cod_cpv, start = 1, end = 2) == "33",
-      true = 1, false = 0),
-    contract_duration = dplyr::if_else(
-      data_effettiva_ultimazione < data_inizio_effettiva,
-      true = NA_real_, false =
-        1 + as.numeric(data_effettiva_ultimazione - data_inizio_effettiva)
-    )
-  ) -> data_out
-
-grand_mean <- data_out %>%
-  dplyr::filter(prepost == "post") %>%
-  dplyr::pull(contract_duration) %>%
-  mean(na.rm = TRUE)
-
-wowo =data_out %>%
-  dplyr::filter(prepost == "post" & !is.na(contract_duration)) %>%
-  # dplyr::filter(flagdivision == 1) %>%  #commented for the moment (very few data)
-  dplyr::group_by(cf_amministrazione_appaltante) %>%
-  dplyr::mutate(
-    ntot = dplyr::n(),
-    mean = mean(contract_duration, na.rm = TRUE),
-    values = list(contract_duration),
-    nnomiss = sum(!is.na(contract_duration)),
-    ttest  = t.test(x = contract_duration[nnomiss!=1], mu =grand_mean)$p.value)
-
-
-
-  generate_indicator_schema(
-    indicator_id = indicator_id,
-    indicator_name = indicator_name,
-    ttest,
-    {{ stat_unit }},
-    aggregation_type = rlang::as_string(aggregation_type),
-    emergency = emergency_name
-  ) %>%
-  dplyr::rename(
-    indicator_value = ttest,
-    aggregation_name = {{ stat_unit }}
-  ) %>%
-  return()
+# indicator_id <- 9
+# indicator_name <- "Lengthy contracts"
+# emergency_scenario <- emergency_dates("coronavirus")
+#
+# mock_data_core %>%
+#   dplyr::mutate(
+#     prepost = dplyr::if_else(
+#       lubridate::ymd(data_pubblicazione) >= emergency_scenario$em_date,
+#       true = "post",false = "pre"),
+#     prepost = forcats::as_factor(prepost),
+#     flagdivision = dplyr::if_else(
+#       stringr::str_sub(cod_cpv, start = 1, end = 2) == "33",
+#       true = 1, false = 0),
+#     contract_duration = dplyr::if_else(
+#       data_effettiva_ultimazione < data_inizio_effettiva,
+#       true = NA_real_, false =
+#         1 + as.numeric(data_effettiva_ultimazione - data_inizio_effettiva)
+#     )
+#   ) -> data_out
+#
+# grand_mean <- data_out %>%
+#   dplyr::filter(prepost == "post") %>%
+#   dplyr::pull(contract_duration) %>%
+#   mean(na.rm = TRUE)
+#
+# wowo =data_out %>%
+#   dplyr::filter(prepost == "post" & !is.na(contract_duration)) %>%
+#   # dplyr::filter(flagdivision == 1) %>%  #commented for the moment (very few data)
+#   dplyr::group_by(cf_amministrazione_appaltante) %>%
+#   dplyr::mutate(
+#     ntot = dplyr::n(),
+#     mean = mean(contract_duration, na.rm = TRUE),
+#     values = list(contract_duration),
+#     nnomiss = sum(!is.na(contract_duration)),
+#     ttest  = t.test(x = contract_duration[nnomiss!=1], mu =grand_mean)$p.value)
+#
+#
+#
+#   generate_indicator_schema(
+#     indicator_id = indicator_id,
+#     indicator_name = indicator_name,
+#     ttest,
+#     {{ stat_unit }},
+#     aggregation_type = rlang::as_string(aggregation_type),
+#     emergency = emergency_name
+#   ) %>%
+#   dplyr::rename(
+#     indicator_value = ttest,
+#     aggregation_name = {{ stat_unit }}
+#   ) %>%
+#   return()
