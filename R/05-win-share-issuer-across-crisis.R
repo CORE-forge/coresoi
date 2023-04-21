bfea73c6db00232e70f9f3723105b74524df3ec3
Simone Del Sarto <simone.delsarto@unipg.it>
2023-04-21 09:58:33 +0200

    updated resilience4

--- R/05-win-share-issuer-across-crisis.R
+++ R/05-win-share-issuer-across-crisis.R
@@ -44,7 +44,7 @@ ind_5 <- function(data,
                   emergency_name) {
   indicator_id <- 5
   indicator_name <- "Winner's share of issuer's contract across the crisis"
-  aggregation_type <- rlang::quo_expr(enquo(stat_unit))
+  aggregation_type <- quo_squash(enquo(stat_unit))
   emergency_scenario <- emergency_dates(emergency_name)
   cpvs <- get_associated_cpv_from_emergency(emergency_scenario$em_name)
   cpv_col <- grab_cpv(data = data)
@@ -58,22 +58,51 @@ ind_5 <- function(data,
       flagdivision = dplyr::if_else(stringr::str_sub(.data[[cpv_col]], start = 1, end = 2) %in% cpvs, 1, 0)
     ) %>%
     dplyr::filter(flagdivision == 1) %>%
-    dplyr::count({{ stat_unit }}, {{ winners }}, prepost) %>%
+    dplyr::filter(!is.na({{ winners }})) %>%
+    # dplyr::count({{ stat_unit }}, {{ winners }}, prepost) %>%
     dplyr::group_by({{ stat_unit }}, prepost) %>%
     dplyr::summarise(
-      norm_gs = DescTools::GiniSimpson({{ winners }} %>% forcats::as_factor()) / ((n() - 1) / n())
+      norm_gs = 1 -
+        DescTools::GiniSimpson({{ winners }} %>% as.factor()) /
+          ((dplyr::n_distinct({{ winners }}) - 1) / dplyr::n_distinct({{ winners }})),
+      norm_gs = round(norm_gs, 10)
     ) %>%
-    ungroup() %>%
+    dplyr::ungroup() %>%
     tidyr::pivot_wider(names_from = prepost, values_from = norm_gs) %>%
+    dplyr::filter(!is.na(pre) & !is.na(post)) %>%
     dplyr::mutate(
-      ratio = post / pre
+      prepost00 = dplyr::if_else(pre == 0 & post == 0,
+        true = 1,
+        false = 0
+      ),
+      diff = post - pre,
+      pre_cat = cut(pre,
+        # breaks = quantile(pre[prepost00 == 0], c(0, 1/5, 2/5, 3/5, 4/5, 1)),
+        breaks = quantile(pre[prepost00 == 0], c(0, 1 / 5, 2 / 5, 3 / 5, 4 / 5, 1)) +
+          c(0, seq_along(1:4) * .Machine$double.eps, 0), # for possible overlapping quantiles
+        labels = 1:5,
+        include.lowest = TRUE
+      ),
+      post_cat = cut(post,
+        # breaks = quantile(post[prepost00 == 0], c(0, 1/5, 2/5, 3/5, 4/5, 1)),
+        breaks = quantile(post[prepost00 == 0], c(0, 1 / 5, 2 / 5, 3 / 5, 4 / 5, 1)) +
+          c(0, seq_along(1:4) * .Machine$double.eps, 0), # for possible overlapping quantiles
+        labels = 1:5,
+        include.lowest = TRUE
+      ),
+      diff_cat = as.numeric(post_cat) - as.numeric(pre_cat),
+      ind5 = dplyr::case_when(
+        diff_cat > 0 ~ 1,
+        diff_cat == 0 & post_cat %in% c(4, 5) & diff > 0 ~ 1,
+        TRUE ~ 0
+      )
     ) %>%
     generate_indicator_schema(
       indicator_id = indicator_id,
       indicator_name = indicator_name,
-      indicator_value = ratio,
+      indicator_value = ind5, # no test
       aggregation_name = {{ stat_unit }},
-      aggregation_type = rlang::as_string(aggregation_type),
+      aggregation_type = as_string(aggregation_type),
       emergency = emergency_scenario
     ) %>%
     return()
